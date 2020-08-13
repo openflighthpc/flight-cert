04794c6c2b8bf00fe7d0a3e39563e4b7a7b26648
William McCumsite <william.mccumstie@gmail.com>
2020-08-13 16:47:38 +0100

    Bump the version to 0.2.1

--- lib/flight_cert/version.rb
+++ lib/flight_cert/version.rb
@@ -25,5 +25,5 @@
 # https://github.com/openflighthpc/flight-cert
 #==============================================================================
 module FlightCert
-  VERSION = '0.2.0'
+  VERSION = '0.2.1'
 end
