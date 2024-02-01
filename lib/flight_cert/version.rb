e4e6cfaf3e5d700374dcc424eb2e4b74d0968c7b
Jack Millard <millard64@hotmail.co.uk>
2024-02-01 10:42:37 +0000

    Bump version

--- lib/flight_cert/version.rb
+++ lib/flight_cert/version.rb
@@ -25,5 +25,5 @@
 # https://github.com/openflighthpc/flight-cert
 #==============================================================================
 module FlightCert
-  VERSION = '0.6.0'
+  VERSION = '0.6.1'
 end
